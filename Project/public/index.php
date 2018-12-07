0db453a5147bef4379ea7ed9dd24bd0515f00d86
andrew <chibarandrey@gmail.com>
2018-12-07 15:15:55 +0300

    with entry point class

--- Project/public/index.php
+++ Project/public/index.php
@@ -1,63 +1,19 @@
 <?php
 
-function loadTemplate($templateFileName, $variables = []) {
-    extract($variables);
-
-    ob_start();
-    include __DIR__ . '/../templates/' . $templateFileName;
-
-    return ob_get_clean();
-}
-
 try {
-    include __DIR__ . '/../includes/DatabaseConnection.php';
-    include __DIR__ . '/../classes/DatabaseTable.php';
+    include __DIR__ . '/../classes/EntryPoint.php';
 
-    $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-    $authorsTable = new DatabaseTable($pdo, 'author', 'id');
 
     $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
 
-    if ($route == strtolower($route)) {
-        if ($route === 'joke/list') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->list();
-        } elseif ($route === '') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->home();
-        } elseif ($route === 'joke/edit') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->edit();
-        } elseif ($route === 'joke/delete') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->delete();
-        } elseif ($route === 'register') {
-            include __DIR__ . '/../classes/controllers/RegisterController.php';
-            $controller = new RegisterController($authorsTable);
-            $page = $controller->showForm();
-        }
-    } else {
-        http_response_code(301);
-        header('location: index.php?route=' . strtolower($route));
-    }
-
-    $title = $page['title'];
-
-    if (isset($page['variables'])) {
-        $output = loadTemplate($page['template'], $page['variables']);
-    } else {
-        $output = loadTemplate($page['template']);
-    }
+    $entryPoint = new EntryPoint( $route );
+    $entryPoint->run();
 
 } catch (PDOException $e) {
     $title = 'An error has occurred';
 
     $output = 'Database error: ' . $e->getMessage() .
         ' in ' . $e->getFile() . ':' . $e->getLine();
-}
 
-include __DIR__ . '/../templates/layout.html.php';
+    include __DIR__ . '/../templates/layout.html.php';
+}
